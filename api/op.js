dca1ebcbb24d68706da7256ea99224b72c2978e7
kaspyX <bogdan.sumskoy@gmail.com>
2026-01-19 14:14:14 +0200

    \

--- api/op.js
+++ api/op.js
@@ -1,24 +1,8 @@
 export default function handler(req, res) {
-  const userAgent = req.headers['user-agent'] || '';
+  // Твой Lua код
+  const luaScript = `print("Synity connection test: SUCCESS")`;
 
-  // Проверяем, есть ли слово Roblox в заголовке
-  if (userAgent.includes('Roblox')) {
-    // ТВОЙ КОД СКРИПТА ТУТ
-    const luaScript = `
-        print("Synity Loaded!")
-        print("hello")
-    `;
-
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    return res.status(200).send(luaScript);
-  } else {
-    // Если зашли через браузер — посылаем куда подальше
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    return res.status(403).send(`
-        <body style="background:#02040a; color:red; display:flex; justify-content:center; align-items:center; height:100vh; font-family:sans-serif;">
-            <h1>403 | ACCESS DENIED</h1>
-            <p style="margin-left:20px; color:gray;">Synity security system: Roblox environment not detected.</p>
-        </body>
-    `);
-  }
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешаем доступ отовсюду
+  return res.status(200).send(luaScript);
 }
